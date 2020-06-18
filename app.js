c40c1af89abb5299f157febf98d18373a0a0a797
nosibah93 <hefa.elayan@gmail.com>
2020-06-18 16:28:18 +0300

    TicTacToe

---
+++ app.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    // document.getElementById("row").style.color ="red"
+//     var x = document.getElementsByClassName("row");
+//   x[0].style.backgroundColor = "red";
+// }
+
+    //document.getElementById("demo").style.color = "red";
+
+    var x = document.getElementById("myTable").rows[0].cells;
+    x[0].innerHTML = "X";
+  }
