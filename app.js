c3008633b03ae5f781d4099a4b9c157735eb59a1
nosibah93 <hefa.elayan@gmail.com>
2020-06-18 17:26:02 +0300

    2 try

--- app.js
+++ app.js
@@ -2,10 +2,28 @@ function myFunction() {
     // document.getElementById("row").style.color ="red"
 //     var x = document.getElementsByClassName("row");
 //   x[0].style.backgroundColor = "red";
-// }
+document.getElementById("index1").textContent = "X"
 
-    //document.getElementById("demo").style.color = "red";
+}
 
-    var x = document.getElementById("myTable").rows[0].cells;
-    x[0].innerHTML = "X";
-  }
+//     //document.getElementById("demo").style.color = "red";
+
+//     // var x = document.getElementById("myTable").rows[0].cells;
+//     // x[0].innerHTML = "X";
+//     
+//   }
+//   function clickHere(){
+
+//     var table = document.getElementById("table");
+//     var row ;
+//     var cell;
+//         for(var i=0;i<2;i++){
+//           row = table.insertRow(i);
+//           row.addEventListener('click', clickHandlerRow);
+//           for(var j=0;j<2;j++){
+//                 cell = row.insertCell(j);
+//                 cell.addEventListener('click', clickHandlerColumn);
+//             }
+//         }
+//     }
+
